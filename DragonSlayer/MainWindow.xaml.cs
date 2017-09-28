9172858e4da71e37a58558d09581da348c8e9401
Andreas Norberg <fiskkneipp@gmial.com>
2017-09-28 13:33:37 +0200

    trams

--- DragonSlayer/MainWindow.xaml.cs
+++ DragonSlayer/MainWindow.xaml.cs
@@ -32,13 +32,6 @@ namespace DragonSlayer
             this.Close();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Leaderboard leaderboard = new Leaderboard();
-            leaderboard.Show();
-            this.Close();
-        }
-
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             this.Close();
